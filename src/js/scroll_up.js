7e36f00e890e8035fadc32e41eccd95bfe67b594
Nata <40355692+merlincat@users.noreply.github.com>
2023-10-04 12:40:33 +0300

    Added scroll_up
    Додала файл js, де вказала поведінку кнопки скролла до гори, коли користувач тільки на першому екрані, додала в хедер id="header", додала у файл order.html розмітку для кнопочки скролла до гори, зазначила їй стилі

---
+++ src/js/scroll_up.js
@@ -0,0 +1,15 @@
+// Get the target:
+let mybutton = document.getElementById('scroll_up');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
